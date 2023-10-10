6927575556221d60faa781b4b27d2e3035afe5d6
Jos Landuré <tyjos29830@gmail.com>
2023-10-10 17:33:33 -0400

    feat: ajout de a*

--- Map.cpp
+++ Map.cpp
@@ -24,6 +24,36 @@ tileindex_t Map::getTileNeighbour(tileindex_t source, kit::DIRECTIONS direction)
     }
 }
 
+std::vector<tileindex_t> Map::getValidNeighbours(tileindex_t source) const
+{
+    static const std::vector<kit::DIRECTIONS> neighbourDirections = { kit::DIRECTIONS::NORTH, kit::DIRECTIONS::EAST, kit::DIRECTIONS::SOUTH, kit::DIRECTIONS::WEST };
+    std::vector<tileindex_t> neighbours{};
+
+    for (kit::DIRECTIONS direction : neighbourDirections) {
+        if (!isValidNeighbour(source, direction)) continue;
+
+        if (tileindex_t neighbourIndex = getTileNeighbour(source, direction)) {
+            Tile tile = tileAt(neighbourIndex);
+            if (tile.getType() != TileType::ENEMY_CITY) neighbours.push_back(neighbourIndex);
+        }
+    }
+
+    return neighbours;
+}
+
+bool Map::isValidNeighbour(tileindex_t source, kit::DIRECTIONS direction) const
+{
+    std::pair<int, int> position = getTilePosition(source);
+
+    switch (direction) {
+    case kit::DIRECTIONS::EAST:  return position.first < m_width;
+    case kit::DIRECTIONS::WEST:  return position.first > 0;
+    case kit::DIRECTIONS::SOUTH: return position.second < m_height;
+    case kit::DIRECTIONS::NORTH: return position.second > 0;
+    default: throw std::runtime_error("Got unexpected direction " + std::to_string((int)direction));
+    }
+}
+
 std::pair<int, int> Map::getTilePosition(tileindex_t tile) const
 {
     return std::make_pair(tile % m_width, tile / m_width);
