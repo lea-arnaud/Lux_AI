71e138a7686efd3658376ac0c0d390beeeb0c58e
Jos Landuré <tyjos29830@gmail.com>
2023-10-22 19:39:05 -0400

    feat: mise en place de la fonction de propagation d'influences

--- InfluenceMap.cpp
+++ InfluenceMap.cpp
@@ -2,50 +2,62 @@
 
 #include <algorithm>
 
-InfluenceMap::InfluenceMap(int width, int height, float initialInfluence) :
-    m_width{ width },
-    m_height{ height }
+InfluenceMap::InfluenceMap(int width, int height) :
+  m_width{ width },
+  m_height{ height }
 {
-	m_map = std::vector<float>(width * height, initialInfluence);
+  m_map = std::vector<float>(width * height, 0.0f);
 }
 
-void InfluenceMap::propagate(int index, float initialInfluence, float decay)
+void InfluenceMap::propagate(int index, float initialInfluence, float (*propagationFunction)(float, float))
 {
+  if (!propagationFunction)
+    propagationFunction = [](float influence, float distance) { return influence - influence*distance/5.0f; };
+
+  int x1 = index % m_width;
+  int y1 = index / m_width;
+
+  for (int i = 0; i < m_map.size(); ++i) {
+    int x2 = i % m_width;
+    int y2 = i / m_width;
+    int distance = std::abs(x1 - x2) + std::abs(y1 - y2);
+    m_map[i] += propagationFunction(initialInfluence, static_cast<float>(distance));
+  }
 }
 
 void InfluenceMap::addMap(const InfluenceMap &influenceMap, float weight)
 {
-    std::transform(m_map.begin(), m_map.end(), influenceMap.getMap().begin(), 
-    [weight](float currentScore, float otherScore) {
-            return currentScore + (otherScore * weight);
-        });
+  std::transform(m_map.begin(), m_map.end(), influenceMap.getMap().begin(), m_map.begin(),
+  [weight](float currentScore, float otherScore) {
+    return currentScore + (otherScore * weight);
+  });
 }
 
 void InfluenceMap::multiplyMap(const InfluenceMap &influenceMap, float weight)
 {
-    std::transform(m_map.begin(), m_map.end(), influenceMap.getMap().begin(), 
-    [weight](float currentScore, float otherScore) {
-            return currentScore * (otherScore * weight);
-        });
+  std::transform(m_map.begin(), m_map.end(), influenceMap.getMap().begin(), m_map.begin(),
+  [weight](float currentScore, float otherScore) {
+    return currentScore * (otherScore * weight);
+  });
 }
 
 void InfluenceMap::normalize()
 {
-    float minVal = *std::min_element(m_map.begin(), m_map.end());
-    float maxVal = *std::max_element(m_map.begin(), m_map.end());
+  float minVal = *std::min_element(m_map.begin(), m_map.end());
+  float maxVal = *std::max_element(m_map.begin(), m_map.end());
 
-    if (minVal == maxVal) return;
+  if (minVal == maxVal) return;
 
-    std::for_each(m_map.begin(), m_map.end(), [minVal, maxVal](float score) { return (score - minVal) / (maxVal - minVal); });
+  std::for_each(m_map.begin(), m_map.end(), [minVal, maxVal](float score) { return (score - minVal) / (maxVal - minVal); });
 }
 
 void InfluenceMap::flip()
 {
-    std::for_each(m_map.begin(), m_map.end(), [](float score) { return 1.0f - score; });
+  std::for_each(m_map.begin(), m_map.end(), [](float score) { return 1.0f - score; });
 }
 
 int InfluenceMap::getHighestPoint()
 {
-    auto maxIterator = std::max_element(m_map.begin(), m_map.end());
-    return std::distance(m_map.begin(), maxIterator);;
+  auto maxIterator = std::max_element(m_map.begin(), m_map.end());
+  return static_cast<int>(std::distance(m_map.begin(), maxIterator));
 }
--- InfluenceMap.h
+++ InfluenceMap.h
@@ -5,22 +5,22 @@
 
 class InfluenceMap
 {
-    int m_width{}, m_height{};
-    std::vector<float> m_map;
+  int m_width{}, m_height{};
+  std::vector<float> m_map;
 
 public:
-    InfluenceMap(int width, int height, float initialInfluence);
+  InfluenceMap(int width, int height);
 
-    std::vector<float> getMap() const { return m_map; };
+  std::vector<float> getMap() const { return m_map; };
 
-    void propagate(int index, float initialInfluence, float decay);
+  void propagate(int index, float initialInfluence, float (*propagationFunction)(float, float) = nullptr);
 
-    void addMap(const InfluenceMap &map, float weight);
-    void multiplyMap(const InfluenceMap &map, float weight);
+  void addMap(const InfluenceMap &map, float weight);
+  void multiplyMap(const InfluenceMap &map, float weight);
 
-    void normalize();
-    void flip();
-    int getHighestPoint();
+  void normalize();
+  void flip();
+  int getHighestPoint();
 };
 
 #endif
