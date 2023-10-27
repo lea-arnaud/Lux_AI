f70f9db838105fc2025a78ae1a63d80af9fe5069
Jos <lanj0504@usherbrooke.ca>
2023-10-27 16:09:55 -0400

    update: ajout de nouvelles fonctions

--- InfluenceMap.cpp
+++ InfluenceMap.cpp
@@ -9,6 +9,13 @@ InfluenceMap::InfluenceMap(int width, int height) :
   m_map = std::vector<float>(width * height, 0.0f);
 }
 
+constexpr InfluenceMap::InfluenceMap(int width, int height, float initialInfluence, float(*propagationFunction)(float, float))
+  : InfluenceMap(width, height)
+{
+  int center = (m_width * m_height - 1) / 2;
+  propagate(center, initialInfluence, propagationFunction);
+}
+
 void InfluenceMap::propagate(int index, float initialInfluence, float (*propagationFunction)(float, float))
 {
   if (!propagationFunction)
@@ -27,7 +34,7 @@ void InfluenceMap::propagate(int index, float initialInfluence, float (*propagat
 
 void InfluenceMap::addMap(const InfluenceMap &influenceMap, float weight)
 {
-  std::transform(m_map.begin(), m_map.end(), influenceMap.getMap().begin(), m_map.begin(),
+  std::transform(m_map.begin(), m_map.end(), influenceMap.m_map.begin(), m_map.begin(),
   [weight](float currentScore, float otherScore) {
     return currentScore + (otherScore * weight);
   });
@@ -35,10 +42,36 @@ void InfluenceMap::addMap(const InfluenceMap &influenceMap, float weight)
 
 void InfluenceMap::multiplyMap(const InfluenceMap &influenceMap, float weight)
 {
-  std::transform(m_map.begin(), m_map.end(), influenceMap.getMap().begin(), m_map.begin(),
+  std::transform(m_map.begin(), m_map.end(), influenceMap.m_map.begin(), m_map.begin(),
   [weight](float currentScore, float otherScore) {
     return currentScore * (otherScore * weight);
   });
+}
+
+constexpr void InfluenceMap::addMapAtIndex(int index, const InfluenceMap &map, float weight)
+{
+  std::pair<int, int> position = getCoord(index);
+  std::pair<int, int> center = map.getCenter();
+  int deltaX = position.first - center.first;
+  int deltaY = position.second- center.second;
+
+  for (int i = 0; i < map.getSize(); ++i) {
+    auto [x, y] = map.getCoord(i);
+    m_map[getIndex(x + deltaX, y + deltaY)] += map.m_map[i] * weight;
+  }
+}
+
+constexpr void InfluenceMap::multiplyMapAtIndex(int index, const InfluenceMap &map, float weight)
+{
+  std::pair<int, int> position = getCoord(index);
+  std::pair<int, int> center = map.getCenter();
+  int deltaX = position.first - center.first;
+  int deltaY = position.second- center.second;
+
+  for (int i = 0; i < map.getSize(); ++i) {
+    auto [x, y] = map.getCoord(i);
+    m_map[getIndex(x + deltaX, y + deltaY)] *= map.m_map[i] * weight;
+  }
 }
 
 void InfluenceMap::normalize()
--- InfluenceMap.h
+++ InfluenceMap.h
@@ -3,24 +3,56 @@
 
 #include <vector>
 
+// TODO: rajouter des verifications
+
+template <class T, std::size_t N>
+   class Tableau {
+   public:
+      using value_type = T;
+      using size_type = std::size_t;
+   private:
+      value_type vals[N];
+   public:
+      constexpr Tableau() : vals{ {} }
+      {
+      }
+      constexpr const value_type& operator[](size_type n) const {
+         return vals[n];
+      }
+      constexpr value_type& operator[](size_type n) {
+         return vals[n];
+      }
+   };
+
 class InfluenceMap
 {
   int m_width{}, m_height{};
   std::vector<float> m_map;
+  //Tableau<float, W * H> m_carte;
 
 public:
-  InfluenceMap(int width, int height);
+  constexpr InfluenceMap(int width, int height);
+  constexpr InfluenceMap(int width, int height, float initialInfluence, float (*propagationFunction)(float, float) = nullptr);
 
+  constexpr int getIndex(int x, int y) const { return x + y * m_width; }
+  constexpr int getSize() const { return m_width * m_height; }
+  constexpr std::pair<int, int> getCenter() const { return {m_width / 2, m_height / 2}; }
+  constexpr std::pair<int, int> getCoord(int index) const { return { index / m_width, index % m_width }; }
   std::vector<float> getMap() const { return m_map; };
 
-  void propagate(int index, float initialInfluence, float (*propagationFunction)(float, float) = nullptr);
+  constexpr void propagate(int index, float initialInfluence, float (*propagationFunction)(float, float) = nullptr);
 
-  void addMap(const InfluenceMap &map, float weight);
-  void multiplyMap(const InfluenceMap &map, float weight);
+  constexpr void addMap(const InfluenceMap &map, float weight = 1.0f);
+  constexpr void multiplyMap(const InfluenceMap &map, float weight = 1.0f);
 
-  void normalize();
-  void flip();
-  int getHighestPoint();
+  constexpr void addMapAtIndex(int index, const InfluenceMap &map, float weight = 1.0f);
+  constexpr void multiplyMapAtIndex(int index, const InfluenceMap &map, float weight = 1.0f);
+
+  constexpr void normalize();
+  constexpr void flip();
+  constexpr int getHighestPoint();
 };
+
+// constexpr InfluenceMap agentInfluence{ 5, 5, 1.0f };
 
 #endif
