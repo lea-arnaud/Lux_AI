679489e32aa172642ba57d4232c2874ee1aa85f1
Léa Arnaud <arnl2801@usherbrooke.ca>
2023-11-27 09:24:21 -0500

    first commit Node and Planner.h

--- build-solution-x64/Planner.h
+++ build-solution-x64/Planner.h
@@ -1,4 +1,21 @@
-#pragma once
+
+#include <vector>
+#include "Action.h"
+
+#ifndef PLANNER_H
+#define PLANNER_H
+
+namespace goap
+{
+
 class Planner
-{};
+{
+public:
+	static std::vector<Action> plan(const GameState &start, const GameState &goal, const std::vector<Action> &actions);
+};
+
+
+}
+
+#endif
 
